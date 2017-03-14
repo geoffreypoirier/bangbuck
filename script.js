7461edafb53a030252e04498c323d540a94ddf60
Geoffrey Poirier <geoffreypoirier@gmail.com>
2017-03-14 00:58:19 -0600

    - stubbed `script.js`

--- script.js
+++ script.js
@@ -1,9 +1,48 @@
+/**
+ * Global Vars
+ */
+let prefs;
+
+let year = 2000;
+let city = 'Denver';
+
+/**
+ * Utils
+ */
+function id(name) {
+    return document.getElementById(name);
+}
+// function exists(value) { return () ? false :true }
+
+
 function init() {
     console.log('script.js: init()');
+
+
+    /**
+     * TODO - run once
+     */
+
+
+    /**
+     * Get options from the options page then go.
+     */
+    chrome.extension.sendMessage({action: 'prefs'}, function (response) {
+
+        if (response !== undefined && response !== null) {
+            prefs = response;
+            year = prefs.year;
+            city = prefs.city;
+        }
+
+        updateFakeUnitsOfMeasurement();
+
+    });
 
 }
 init();
 
 function updateFakeUnitsOfMeasurement() {
+    console.log('update fake units of measurement');
 
 }
